5116e64ec9dffc6c47522c442877fdb66f99b07f
bogdan demchenko <bogdandemchenko@gmail.com>
2020-12-13 22:34:51 +0200

    request id middleware

--- internal/app/apiserver/server.go
+++ internal/app/apiserver/server.go
@@ -6,6 +6,7 @@ import (
 	"errors"
 	"github.com/4johndoe/http-rest-api/internal/app/model"
 	"github.com/4johndoe/http-rest-api/internal/app/store"
+	"github.com/google/uuid"
 	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/gorilla/sessions"
@@ -15,6 +16,7 @@ import (
 const (
 	sessionName        = "gopherschool"
 	ctxKeyUser  ctxKey = iota
+	ctxKeyRequestID
 )
 
 var (
@@ -50,6 +52,7 @@ func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 // configureRouter ...
 func (s *server) configureRouter() {
+	s.router.Use(s.setRequestID)
 	s.router.Use(handlers.CORS(handlers.AllowedOrigins([]string{"*"})))
 	s.router.HandleFunc("/users", s.handleUsersCreate()).Methods("POST")
 	s.router.HandleFunc("/sessions", s.handleSessionsCreate()).Methods("POST")
@@ -57,6 +60,14 @@ func (s *server) configureRouter() {
 	private := s.router.PathPrefix("/private").Subrouter()
 	private.Use(s.authenticateUser)
 	private.HandleFunc("/whoami", s.handleWhoami()).Methods("GET")
+}
+
+func (s *server) setRequestID(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := uuid.New().String()
+		w.Header().Set("X-Request-ID", id)
+		next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), ctxKeyRequestID, id)))
+	})
 }
 
 func (s *server) authenticateUser(next http.Handler) http.Handler {
