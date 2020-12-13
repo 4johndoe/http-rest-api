064a052a8d9d8aca4136769652dda5f4ff475ead
bogdan demchenko <bogdandemchenko@gmail.com>
2020-12-13 13:07:17 +0200

    user testCAses, validation, gitignore

--- internal/app/store/userrepository.go
+++ internal/app/store/userrepository.go
@@ -9,12 +9,20 @@ type UserRepository struct {
 
 // Create ...
 func (r *UserRepository) Create(u *model.User) (*model.User, error) {
+	if err := u.Validate(); err != nil {
+		return nil, err
+	}
+
+	if err := u.BeforeCreate(); err != nil {
+		return nil, err
+	}
+
 	if err := r.store.db.QueryRow(
 		"INSERT INTO users (email, encrypted_password) VALUES ($1, $2) RETURNING  id",
 		u.Email,
 		u.EncryptedPassword,
-		).Scan(&u.ID); err != nil {
-			return nil, err
+	).Scan(&u.ID); err != nil {
+		return nil, err
 	}
 
 	return u, nil
@@ -25,8 +33,8 @@ func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
 	u := &model.User{}
 	if err := r.store.db.QueryRow(
 		"SELECT id, email, users.encrypted_password FROM users WHERE email = $1", email,
-		).Scan(&u.ID, &u.Email, &u.EncryptedPassword,); err != nil {
-			return nil, err
+	).Scan(&u.ID, &u.Email, &u.EncryptedPassword); err != nil {
+		return nil, err
 	}
 
 	return u, nil
